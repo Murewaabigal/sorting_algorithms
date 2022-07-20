81498cf61d10835c4474b087df9e9e3b9c040652
root <murewaabigal>
2022-07-19 20:31:17 -0700

    commt

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
-#ifndef _SORT_H_
-#define _SORT_H_
-#include <stddef.h>
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -16,21 +18,35 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void counting_sort(int *array, size_t size);
+size_t list_len(listint_t *list);
+void switch_nodes(listint_t **list, listint_t **p);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge_recursion(int *arr, int *array, size_t left, size_t right);
+void merge_subarray(int *arr, int *array, size_t left,
+		size_t middle, size_t right);
 void heap_sort(int *array, size_t size);
+void max_heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
+void count_sort_LSD(int *array, size_t size, size_t lsd);
 void bitonic_sort(int *array, size_t size);
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
+void bitonic_merge(int *array, int l, int r, int direction);
 void quick_sort_hoare(int *array, size_t size);
-
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 
 #endif
